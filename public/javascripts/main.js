1ae79a6fb153f323627b102d15ffce958ed64d11
Alexander Gudulin <alexandr.gudulin@gmail.com>
2014-08-10 21:12:13 +0400

    A lot of stylesheets stuff

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,8 +3,12 @@ $(function(){
 
   var cachedSeasonEpisodesMap = {};
   var $seasonButton = $(".btn[data-season]");
+  var $beemoVisionImgBlock = $(".beemo-vision");
 
-  $seasonButton.on("click", function(){
+  $seasonButton.on("click", function() {
+    $seasonButton.removeClass("active");
+    $(this).addClass("active");
+    $beemoVisionImgBlock.hide();
     var selectedSeason = $(this).data("season");
     var AT_EPISODES_PER_SEASON = "/api/show/" + AT_SHOW_ID + "/" + selectedSeason + "/episodes";
     if (cachedSeasonEpisodesMap[selectedSeason] === undefined) {
@@ -30,11 +34,25 @@ $(function(){
 
     data.forEach(function(episode){
       var $episode = $("<li>");
-      $episode.text(episode.name);
-      episode.videos.forEach(function(video){
-        $episode.append($("<a>").attr("href", video.url).text(video.name))
+      var $episodeName = $("<span>");
+      var $episodeLinks = $("<div>");
+      $episodeName
+        .addClass("episode-name")
+        .text(episode.name);
+      $episodeLinks
+        .addClass("episode-links");
+      episode.videos.forEach(function(video, idx) {
+        $link = $("<a>");
+        $link
+          .addClass("episode-video-link")
+          .attr("href", video.url)
+          .text(video.name);
+        $episodeLinks.append($link);
       });
 
+      $episode
+        .append($episodeName)
+        .append($episodeLinks);
       $episodesNavbar.append($episode);
     });
   }
