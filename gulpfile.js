1826a930e2e1ed4e64e72c47d1eadeac2d227df7
Alexander Gudulin <alexandr.gudulin@gmail.com>
2014-08-10 21:24:16 +0400

    Added minfier/uglifier

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var plumber = require('gulp-plumber');
 var prefix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+var uglifyJS = require('gulp-uglifyjs');
 
 gulp.task('less', function() {
   gulp.src('public/stylesheets/main.less')
       .pipe(plumber())
       .pipe(less())
       .pipe(prefix({ cascade: true }))
+      .pipe(minifyCSS({ keepBreaks: false }))
       .pipe(gulp.dest('public/stylesheets'));
 });
 
@@ -15,4 +18,10 @@ gulp.task('watch', function() {
   gulp.watch('public/stylesheets/*.less', ['less']);
 });
 
-gulp.task('default', ['less', 'watch']);
+gulp.task('uglifyjs', function() {
+  gulp.src('public/javascripts/*/*.js')
+    .pipe(uglifyJS())
+    .pipe(gulp.dest('public/js'))
+});
+
+gulp.task('default', ['less', 'watch', 'uglifyjs']);
