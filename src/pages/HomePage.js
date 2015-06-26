f4b807c7bf5dbb6e95908c85049c363952901259
Alexander Gudulin <alexandr.gudulin@gmail.com>
2015-06-26 21:50:29 +0300

    add navigation component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 
 import PageLayout from "../components/PageLayout";
+import SeasonNavigation from "../components/SeasonNavigation";
 
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seasons: [1, 2, 3, 4, 5, 6]
+    };
+  }
 
   render() {
     return (
       <PageLayout updatedDateTime="just now">
-        page content
+        <aside>
+          <SeasonNavigation seasons={this.state.seasons} />
+        </aside>
+
+        <article>
+          content
+        </article>
       </PageLayout>
     );
   }
