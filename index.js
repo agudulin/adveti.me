b14a8707edc71d45b71d61b6b7f1d79eb7524c8d
Alexander Gudulin <alexandr.gudulin@gmail.com>
2015-06-27 13:46:31 +0300

    prod configs

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ require("babel/register")({
   plugins: ["typecheck"]
 });
 
-// Define universal constants
-delete global.__BROWSER__;
-
 if (process.env.NODE_ENV !== "production") {
-  if (!require("piping")({
-      hook: true,
-      ignore: /(\/\.|~$|\.json|\.css$)/i
-    })) {
-    require("./src/server");
+  if (require("piping")({ hook: true, ignore: /(\/\.|~$|\.json|\.css$)/i})) {
+    return;
   }
 }
+require("./src/server");
